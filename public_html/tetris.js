3a47238c4b46c32d1f9719315e04499152e5d674
Tsuki <Tsuki@192.168.12.92>
2018-12-05 20:08:34 +0100

    aa

--- public_html/tetris.js
+++ public_html/tetris.js
@@ -1,74 +1,125 @@
-/*
-	I = cyan
-	J = blue
-	L = orange
-	O = yellow
-	S = green
-	T = purple
-	Z = red
-*/
 var game = { 
 	board: null,
+	lengthB: [22, 12],
 	nextPiece: null,
 	points: null,
-	countPieces: {"i" : null,"j" : null,"l" : null,"o" : null}, // I, J, L, O, S, T, Z
-	//countPieces: [null, null, null, null, null, null, null], // I, J, L, O, S, T, Z
-	dades: function() // metodes : funció,
-	{ return(this.nom + "-" + this.ocupacio + "-" + this.edat);
+	countP: null,
+			//// I, J, L, O, S, T, Z
+	init: function() { // metodes : funció,
+		this.board = new Array (this.lengthB[0]);
+		for (var i=0 ; i < this.lengthB[0] ; i++) {
+			this.board[i] = new Array (this.lengthB[1]);
+			this.board[i][0]=1;
+			for (var j=1 ; j < this.lengthB[1] ; j++) {
+				this.board[i][j]=0;
+			}
+			this.board[i][this.lengthB[1]-1]=1;
+		}
+		for (var j=1 ; j < this.lengthB[1] ; j++) {
+			this.board[this.lengthB[0]-1][j]=1;
+		}
+		this.points = 0;
+		this.countP = new Array ();
+			this.countP["i"] = 0;
+			this.countP["j"] = 0;
+			this.countP["l"] = 0;
+			this.countP["o"] = 0;
+			this.countP["s"] = 0;
+			this.countP["t"] = 0;
+			this.countP["z"] = 0;
+		this.nextPiece = piece.new();
 	}
 }; 
- 
-// 0 1 y
-// 1 
-// x
-var pieza = function(color, forma, posX, posY)
-	{	this.color = color;
-		this.forma = forma;
-		this.posicion = [posX, posY];
-		this.moverX = function(x) {
+
+var piece = function(name, form, posX, posY)
+	{	this.name = name;
+		this.form = form;
+		this.pos = [posX, posY];
+		
+		this.move = function(x) {
 			if (x==-1 || x==1) {
-				this.posicion[0] = parseInt(this.posicion[0])+x;
+				this.pos[0] = parseInt(this.pos[0])+x;
 			}
-		}
-		this.bajar = function() {
-			this.posicion[1] = parseInt(this.posicion[1])+1;
-		}
-		this.girar = function() {
+		};
+		this.godown = function() {
+			this.pos[1] = parseInt(this.pos[1])+1;
+		};
+		this.turn = function() {
 			//var tempForma = new Array(this.forma[0].length);
-			var tempForma = [
+			var tempForm = [
 					[0, 0, 0, 0],
 					[0, 0, 0, 0],
 					[0, 0, 0, 0],
 					[0, 0, 0, 0]
 				];
 			
-			for (var i=0; i < this.forma.length ; i++) {
+			for (var i=0; i < this.form.length ; i++) {
 				//tempForma[i] = new Array(this.forma.length);
-				for (var j = 0; j < this.forma[i].length ; j++) {
-					tempForma[j][ this.forma[i].length-i-1 ] = this.forma[i][j];
+				for (var j = 0; j < this.form[i].length ; j++) {
+					tempForm[j][ this.form[i].length-i-1 ] = this.form[i][j];
 				}
 			}
-			this.forma=tempForma;
-		}
-		this.mostrar = function() {
+			this.form=tempForm;
+		};
+		this.show = function() {
 			var out="";
-			out = out+"Color: "+this.color;
-			out = out+"<br>Forma: ";
-			for (var i=0; i < this.forma.length ; i++) {
+			out = out+"Name: "+this.name;
+			out = out+"<br>Form: ";
+			for (var i=0; i < this.form.length ; i++) {
 				out = out+"<br>";
-				for (var j = 0; j < this.forma[i].length; j++) {
-					if (this.forma[i][j]==1) {
-						out = out+"<span style=\"color:"+this.color+"\">&#9724;</span>";
+				for (var j = 0; j < this.form[i].length; j++) {
+					if (this.form[i][j]==1) {
+						out = out+"<span class="+ this.name +">&#9724;</span>";
 
 					} else {
 						out = out+"&#9724;";
 					}
 				}
 			}
-			out = out+"<br>PosisciónX: "+this.posicion[0];
-			out = out+"<br>PosisciónY: "+this.posicion[1];
+			out = out+"<br>PosisciónX: "+this.pos[0];
+			out = out+"<br>PosisciónY: "+this.pos[1];
 			document.getElementById("resultado").innerHTML = out;
-		}
+		};
+		/*this.randomP = function() { 
+			return parseInt(Math.random() * 7);
+		};*/
+		this.newP = function() {
+			var i = parseInt(Math.random() * 7);
+			var name = null;
+			var forma = null;
+			switch (i) {
+				case 0:
+					name = "i";
+					forma = [[0, 1, 0, 0],[0, 1, 0, 0],[0, 1, 0, 0],[0, 1, 0, 0]];
+					break;
+				case 1:
+					name = "j";
+					forma = [[0, 0, 1, 0],[0, 0, 1, 0],[0, 1, 1, 0],[0, 0, 0, 0]];
+					break;
+				case 2:
+					name = "l";
+					forma = [[0, 1, 0, 0],[0, 1, 0, 0],[0, 1, 1, 0],[0, 0, 0, 0]];
+					break;
+				case 3:
+					name = "o";
+					forma = [[0, 0, 0, 0],[0, 1, 1, 0],[0, 1, 1, 0],[0, 0, 0, 0]];
+					break;
+				case 4:
+					name = "z";
+					forma = [[0, 0, 0, 0],[1, 1, 0, 0],[0, 1, 1, 0],[0, 0, 0, 0]];
+					break;
+				case 5:
+					name = "t";
+					forma = [[0, 0, 0, 0],[1, 1, 1, 0],[0, 1, 0, 0],[0, 0, 0, 0]];
+					break;
+				case 6:
+					name = "s";
+					forma = [[0, 0, 0, 0],[0, 1, 1, 0],[1, 1, 0, 0],[0, 0, 0, 0]];
+					break;
+				default: alert("error genera nombre de pessa");
+			}	
+			return new piece(name, forma, 0, 6);
+		};
 };
 var  forma = [
 	[0, 1, 0, 0],
@@ -76,24 +127,31 @@ var  forma = [
 	[0, 1, 1, 0],
 	[0, 0, 0, 0],
 ];
-var piezaej = new pieza('blue', forma, '5', '5'); 
+var piezaej = new piece('i', forma, '5', '5'); 
+piezaej = piezaej.newP();
 
-function myFunctionMostar() {
-	piezaej.mostrar();
+game.init();
+
+function myFunctionMostrar() {
+	piezaej.show();
 }
 function myFunctionGirar() {
-	piezaej.girar();
-	piezaej.mostrar();
+	piezaej.turn();
+	piezaej.show();
 }
 function myFunctionBajar() {
-	piezaej.bajar();
-	piezaej.mostrar();
+	piezaej.godown();
+	piezaej.show();
 }
 function myFunctionMoverI() {
-	piezaej.moverX(-1);
-	piezaej.mostrar();
+	piezaej.move(-1);
+	piezaej.show();
 }
 function myFunctionMoverD() {
-	piezaej.moverX(1);
-	piezaej.mostrar();
+	piezaej.move(1);
+	piezaej.show();
+}
+function myFunctionNewPiece() {
+	piezaej = piezaej.newP();
+	piezaej.show();
 }
